8effc572bafbd3689afbd43ad094bb3ceac16fe3
acharya <srikrishna3118@gmail.com>
2020-02-20 20:55:20 +0530

    update net_init.sh to install all packages

--- network/net_init.sh
+++ network/net_init.sh
@@ -4,6 +4,37 @@
 wget https://www.nsnam.org/releases/ns-allinone-3.29.tar.bz2
 tar xvf ns-allinone*
 
+sudo apt-get install gcc g++ python python3 python3-dev -y
+sudo apt-get install python3-setuptools git mercurial -y 
+sudo apt-get install qt5-default mercurial -y
+sudo apt-get install gir1.2-goocanvas-2.0 python-gi python-gi-cairo python-pygraphviz python3-gi python3-gi-cairo python3-pygraphviz gir1.2-gtk-3.0 ipython ipython3 -y
+sudo apt-get install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev -y
+sudo apt-get install autoconf cvs bzr unrar -y
+sudo apt-get install gdb valgrind -y
+sudo apt-get install uncrustify -y
+sudo apt-get install doxygen graphviz imagemagick -y
+sudo apt-get install texlive texlive-extra-utils texlive-latex-extra texlive-font-utils texlive-lang-portuguese dvipng latexmk -y
+sudo apt-get install python3-sphinx dia -y
+sudo apt-get install gsl-bin libgsl-dev libgsl23 libgslcblas0 -y
+sudo apt-get install tcpdump -y
+sudo apt-get install sqlite sqlite3 libsqlite3-dev -y 
+sudo apt-get install libxml2 libxml2-dev -y
+sudo apt-get install cmake libc6-dev libc6-dev-i386 libclang-6.0-dev llvm-6.0-dev automake -y
+pip install cxxfilt
+sudo apt-get install libgtk2.0-0 libgtk2.0-dev -y
+sudo apt-get install vtun lxc uml-utilities -y
+sudo apt-get install libboost-signals-dev libboost-filesystem-dev -y
+
+
+#install libzmq libraries
+sudo apt-get install libzmq5 libzmq3-dev libczmq-dev libczmq4 libxml2 libxml2-dev
+
+#for gui
+sudo apt-get install python-qt4
+sudo apt install xterm
+sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
+
+
 HOME_DIR=$(pwd)
 echo $HOME_DIR
 NS3_HOME=$HOME_DIR/ns-allinone-3.29/ns-3.29
